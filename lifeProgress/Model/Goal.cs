9418430533eac0c57f8888bbfaa7f3d3f8dfbea9
dralxs <dr.ales@protonmail.com>
2024-02-27 20:38:13 +0100

    first commit

---
+++ lifeProgress/Model/Goal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeProgress.Model
+{
+    public enum Difficulties
+    {
+        VeryEasy,
+        Easy,
+        Medium,
+        Hard,
+        VeryHard
+    }
+
+    public class Goal : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public Goal(string name)
+        {
+            _name = name;
+        }
+
+        private Difficulties _difficulty;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Difficulties Difficulty
+        {
+            get => _difficulty;
+            set
+            {
+                _difficulty = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        // Notify the binding that the property has changed
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
